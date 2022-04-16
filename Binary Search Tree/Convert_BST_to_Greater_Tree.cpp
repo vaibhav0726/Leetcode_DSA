ed3719a55fff9777c9d3bfe0c592892c53682ba9
Vaibhav  Varshney <vaibhav.varshney2_cs20@gla.ac.in>
2022-04-16 13:04:17 +0530

    538

--- Binary Search Tree/Convert_BST_to_Greater_Tree.cpp
+++ Binary Search Tree/Convert_BST_to_Greater_Tree.cpp
@@ -1,6 +1,7 @@
 // Given the root of a Binary Search Tree (BST), convert it to a Greater Tree such that 
 // every key of the original BST is changed to the original key plus the sum of all keys greater than the original key in BST.
 
+// time:- O(n) for traversing every node space:- O(n) for recursion stack
 class Solution {
 public:
     int sum = 0;
