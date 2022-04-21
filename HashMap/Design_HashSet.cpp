c2a27bf1f6318400fe9451491ccdd7a7fd0928aa
Vaibhav  Varshney <vaibhav.varshney2_cs20@gla.ac.in>
2022-04-21 19:58:07 +0530

    705

---
+++ HashMap/Design_HashSet.cpp
@@ -0,0 +1,26 @@
+// Design a HashSet without using any built-in hash table libraries.
+// Implement MyHashSet class:
+// void add(key) Inserts the value key into the HashSet.
+// bool contains(key) Returns whether the value key exists in the HashSet or not.
+// void remove(key) Removes the value key in the HashSet. If key does not exist in the HashSet, do nothing.
+
+class MyHashSet {
+private:
+    vector<bool> v;
+public:
+    MyHashSet() {
+        v.resize(1e6+1,false);
+    }
+    
+    void add(int key) {
+        v[key] = true;
+    }
+    
+    void remove(int key) {
+        v[key] = false;
+    }
+    
+    bool contains(int key) {
+        return v[key];
+    }
+};
