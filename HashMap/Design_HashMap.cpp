83e0415ac3a07f8edbdc20499038a279f7b6783f
Vaibhav  Varshney <vaibhav.varshney2_cs20@gla.ac.in>
2022-04-22 22:46:40 +0530

    706

---
+++ HashMap/Design_HashMap.cpp
@@ -0,0 +1,48 @@
+// Design a HashMap without using any built-in hash table libraries.
+// Implement the MyHashMap class:
+// MyHashMap() initializes the object with an empty map.
+// void put(int key, int value) inserts a (key, value) pair into the HashMap. If the key already exists in the map, update the corresponding value.
+// int get(int key) returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key.
+// void remove(key) removes the key and its corresponding value if the map contains the mapping for the key.
+
+class MyHashMap {
+    vector<list<pair<int,int>>> m_data;
+    size_t m_size = 10000;
+public:
+    /** Initialize your data structure here. */
+    MyHashMap() {
+        m_data.resize(m_size);
+    }
+    
+    /** value will always be non-negative. */
+    void put(int key, int value) {
+        auto &list = m_data[key % m_size];
+        for (auto & val : list) {
+            if (val.first == key) {
+                val.second = value;
+                return;
+            }
+        }
+        list.emplace_back(key, value);
+    }
+    
+    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+    int get(int key) {
+        const auto &list = m_data[key % m_size];
+        if (list.empty()) {
+            return -1;
+        }
+        for (const auto & val : list) {
+            if (val.first == key) {
+                return val.second;
+            }
+        }
+        return -1;
+    }
+    
+    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
+    void remove(int key) {
+        auto &list = m_data[key % m_size];
+        list.remove_if([key](auto n) { return n.first == key; });
+    }
+};
