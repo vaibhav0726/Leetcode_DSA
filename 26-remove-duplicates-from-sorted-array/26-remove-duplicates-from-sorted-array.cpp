6e52b91eb2e6362ebe844f33d3062cacea428afc
Vaibhav Varshney <vaibhav.varshney2_cs20@gla.ac.in>
2022-09-02 10:15:00 +0530

    Time: 19 ms (57.00%), Space: 18.4 MB (36.85%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& A) {
-        int n = A.size();
         int count = 0;
-for(int i = 1; i < n; i++){
-    if(A[i] == A[i-1]) count++;
-    else A[i-count] = A[i];
-}
-return n-count;
+        int n = A.size();
+        for(int i = 1; i<n; i++){
+            if(A[count] != A[i]){
+                A[++count] = A[i];
+            }
+        }
+        return count+1;
     }
 };
