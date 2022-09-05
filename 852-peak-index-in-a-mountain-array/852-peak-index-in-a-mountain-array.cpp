14a142ce5b555380f5b17f43c81eb51a8888ad68
Vaibhav Varshney <vaibhav.varshney2_cs20@gla.ac.in>
2022-09-05 19:14:49 +0530

    Time: 177 ms (51.94%), Space: 59.7 MB (17.05%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
@@ -1,20 +1,34 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
+//         my approach
+        // int n = arr.size();
+        // int s = 1, e = n-1;
+        // while(s <= e){ 
+        //     int mid = s + ((e-s)/2);
+        //     if(arr[mid] > arr[mid-1] && arr[mid] > arr[mid+1]){
+        //         return mid;
+        //     }
+        //     else if(arr[mid] > arr[mid-1] && arr[mid] < arr[mid+1]){
+        //         s = mid+1;
+        //     }
+        //     else{
+        //         e = mid-1;
+        //     }
+        // }
+        // return 0;
+        
+//         babbar approach
         int n = arr.size();
-        int s = 1, e = n-1;
-        while(s <= e){ 
-            int mid = s + ((e-s)/2);
-            if(arr[mid] > arr[mid-1] && arr[mid] > arr[mid+1]){
-                return mid;
-            }
-            else if(arr[mid] > arr[mid-1] && arr[mid] < arr[mid+1]){
+        int s = 0, e = n-1;
+        while(s < e){
+            int mid = s+((e-s)/2);
+            if(arr[mid] < arr[mid+1])
                 s = mid+1;
-            }
-            else{
-                e = mid-1;
-            }
+            else
+                e = mid;
         }
-        return 0;
+        return s;
+        
     }
 };
