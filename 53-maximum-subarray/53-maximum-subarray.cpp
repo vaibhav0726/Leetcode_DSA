c75d9cdbd45550798d3a60770fc2cced1f32fd00
Vaibhav Varshney <vaibhav.varshney2_cs20@gla.ac.in>
2022-09-14 11:44:04 +0530

    Time: 222 ms (22.00%), Space: 67.7 MB (53.69%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxEnding = nums[0];
+        int res = nums[0];
+        for(int i=1; i<nums.size(); i++){
+            maxEnding = max(maxEnding + nums[i], nums[i]);
+            res = max(res, maxEnding);
+        }
+        return res;
+    }
+};
