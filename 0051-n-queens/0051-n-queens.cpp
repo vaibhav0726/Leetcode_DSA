c2dd57a65bc3a29b2db8d5de5104eca3e2eeb09d
Vaibhav Varshney <vaibhav.varshney2_cs20@gla.ac.in>
2022-10-23 23:55:43 +0530

    Time: 18 ms (37.24%), Space: 7.2 MB (66.70%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.cpp
@@ -0,0 +1,62 @@
+class Solution {
+private:
+//     tq = total queen
+    void printPath(vector<vector<string>> &ans, vector<string> &nQueens, int row, int n){
+        
+        if(row == n){
+            ans.push_back(nQueens);
+            return;
+        }
+        
+        for(int col=0; col < n; col++){
+            if(isitPossible(nQueens, row, col, n)){
+                nQueens[row][col] = 'Q';
+                printPath(ans, nQueens, row+1, n);
+                nQueens[row][col] = '.';
+            }
+        }
+        return;
+    }
+    
+    bool isitPossible(vector<string> &nQueens, int row, int col, int n){
+        int r = row;
+        int c = col;
+//         uper jana hai
+        
+        while(r >= 0){
+            if(nQueens[r][c] == 'Q')
+                return false;
+            r--;
+        }
+        
+        // left diagonal
+        r = row;
+        c = col;
+        while(r >=0 && c>=0){
+            if(nQueens[r][c] == 'Q')
+                return false;
+            r--;
+            c--;
+        }
+        
+        // right diagonal
+        r = row;
+        c = col;
+        while(r >=0 && c<n){
+            if(nQueens[r][c] == 'Q')
+                return false;
+            r--;
+            c++;
+        }
+        
+        return true;
+        
+    }
+public:
+    vector<vector<string>> solveNQueens(int n) {        
+        vector<vector<string>> ans;
+        vector<string> nQueens(n, string(n, '.'));
+        printPath(ans, nQueens, 0, n);
+        return ans;
+    }
+};
