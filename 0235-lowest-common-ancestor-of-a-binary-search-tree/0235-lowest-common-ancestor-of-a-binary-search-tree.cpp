f8391d8c4927d10d81d2c5cb223746f98af2524e
Vaibhav Varshney <vaibhav.varshney2_cs20@gla.ac.in>
2022-10-26 16:56:58 +0530

    Time: 76 ms (13.15%), Space: 23.4 MB (17.69%) - LeetHub

---
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+private:
+    bool getPath(TreeNode* root, TreeNode* p, vector<int> &ans){
+        if(root == NULL) return false;
+        ans.push_back(root -> val);
+        if(root -> val == p -> val) return true;
+        if(getPath(root->left, p, ans) || getPath(root->right, p, ans)){
+            return true;
+        }
+        ans.pop_back();
+        return false;
+    }
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        vector<int> p1;
+        vector<int> p2;
+        TreeNode* temp = root;
+        getPath(temp, p, p1);
+        temp = root;
+        getPath(temp, q, p2);
+        // for(int i=0; i<p1.size(); i++){
+        //     cout<<p1[i]<<" ";
+        // }
+        // cout<<"yes"<<endl;
+        // for(int i=0; i<p2.size(); i++){
+        //     cout<<p2[i]<<" ";
+        // }
+        // cout<<"no"<<endl;
+        TreeNode* ans = NULL;
+        for(int i=0; i<p1.size() && i<p2.size(); i++){
+            if(p1[i] == p2[i]) {
+                ans = new TreeNode(p1[i]);
+            }
+        }
+        return ans;
+    }  
+};
