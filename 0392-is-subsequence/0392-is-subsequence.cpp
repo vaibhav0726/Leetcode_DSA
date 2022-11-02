17354651c2b7582062b6c39a285508aefaaa7942
Vaibhav Varshney <vaibhav.varshney2_cs20@gla.ac.in>
2022-11-02 22:52:12 +0530

    Time: 4 ms (43.01%), Space: 6.4 MB (23.48%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        int j= 0;
-        for(int i=0; i<t.length(); i++){
-            if(j<s.length() && t[i] == s[j]){
+        int j = 0;
+        for(int i=0; i < t.length(); i++){
+            if(j < s.length() && t[i] == s[j]){
                 j++;
             }
         }
-        if(j == s.length() ) return true;
+        if(j == s.length()) return true;
         else return false;
     }
 };
