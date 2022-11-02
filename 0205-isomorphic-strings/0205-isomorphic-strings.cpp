023048da6dfe9846e242a1033251f9cabe9f9b7a
Vaibhav Varshney <vaibhav.varshney2_cs20@gla.ac.in>
2022-11-02 22:49:16 +0530

    Time: 6 ms (90.43%), Space: 6.8 MB (98.25%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        char freq[256]={};
-        // memset(freq, ' ', 256);
+        char freq[256] = {};
         for(int i=0; i<s.length(); i++){
             if(!freq[s[i]]){
-                for(char c:freq){
+                for(auto c: freq){
                     if(c == t[i]) return false;
                 }
                 freq[s[i]] = t[i];
             }
             else if(freq[s[i]] != t[i])
                 return false;
-            cout<<s[i]<<endl;
         }
         return true;
     }
