5945d19dd8dde5285728198726923ffd18c66894
Vaibhav Varshney <vaibhav.varshney2_cs20@gla.ac.in>
2022-11-06 22:35:38 +0530

    Time: 36 ms (74.19%), Space: 8.9 MB (89.88%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,27 +1,23 @@
 class Solution {
 private:
-    void solve(vector<int> &ques, vector<vector<int>> &ans, vector<int> v, int k, int idx){
-        if(k == 0){
-            ans.push_back(v);
-            return;
-        }
-        
-        for(int i = idx; i<ques.size(); i++){
-            v.push_back(ques[i]);
-            solve(ques, ans, v, k-1, i+1);
-            v.pop_back();
-        }
-    }
+   void solve(vector<vector<int>> &ans, int n, int k, int idx, vector<int> &v){
+       if(k == 0){
+           ans.push_back(v);
+           return;
+       }
+       
+       for(int i=idx; i<=n; i++){
+           v.push_back(i);
+           solve(ans, n, k-1, i+1, v);
+           v.pop_back();
+       }
+   }
 
 public:
     vector<vector<int>> combine(int n, int k) {
         vector<vector<int>> ans;
-        vector<int> ques;
         vector<int> v;
-        for(int i=1; i<=n; i++){
-            ques.push_back(i);
-        }
-        solve(ques, ans, v, k, 0);
+        solve(ans, n, k, 1, v);
         return ans;
     }
 };
