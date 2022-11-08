3fed23f902b815a677769fc15069043f5439e308
Vaibhav Varshney <vaibhav.varshney2_cs20@gla.ac.in>
2022-11-08 11:51:21 +0530

    Time: 15 ms (66.43%), Space: 7 MB (98.20%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        if(s1.length() > s2.length()) return false;
+        int count[26] = {0};
+        for(auto c: s1) count[c-'a']++;
+        int i=0, j=0, total_chars = s1.size();
+        while(j < s2.length()){
+            if(count[s2[j++] - 'a']-- > 0){
+                total_chars--;
+            }
+            if(total_chars == 0) return true;
+            if(j-i == s1.length() && count[s2[i++] - 'a']++ >= 0){
+                total_chars++;
+            }
+        }
+        return false;
+    }
+};
