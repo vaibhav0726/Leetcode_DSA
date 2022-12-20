cdcc7c45c46c5362f642d955ce88ea51bd039c0c
Vaibhav Varshney <vaibhav.varshney2_cs20@gla.ac.in>
2022-12-20 23:37:35 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (29.55%) - LeetHub

--- 2413-smallest-even-multiple/2413-smallest-even-multiple.cpp
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
     int smallestEvenMultiple(int n) {
-        if(n%2==0){
-            return n;
-        }
-        else{
-            return n*2;
-        }
+        // if(n%2==0){
+        //     return n;
+        // }
+        // else{
+        //     return n*2;
+        // }
+        
+        return (n << (n & 1));
+        
     }
 };
