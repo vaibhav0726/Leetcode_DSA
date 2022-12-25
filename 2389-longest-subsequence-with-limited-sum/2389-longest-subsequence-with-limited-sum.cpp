81eee279ea09ef70ef2f2f1dd8daae783610a327
Vaibhav Varshney <vaibhav.varshney2_cs20@gla.ac.in>
2022-12-25 16:59:55 +0530

    Time: 19 ms (92.50%), Space: 13.9 MB (36.20%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.cpp
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.cpp
@@ -3,19 +3,12 @@ public:
     vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
         sort(nums.begin(), nums.end());
         vector<int> ans;
+        for(int i=1; i<nums.size(); i++){
+            nums[i] = nums[i-1] + nums[i];
+        }
         for(int i=0; i<queries.size(); i++){
-            int j=0;
-            int sum = 0;
-            while(j<nums.size() && sum + nums[j] <= queries[i]){
-                sum += nums[j++];
-            }
-            // if(sum == queries[i] || j==nums.size()){
-            //     ans.push_back(j);
-            // }
-            // else{
-            //     ans.push_back(j-1);
-            // }
-            ans.push_back(j);
+            int idx = upper_bound(nums.begin(), nums.end(), queries[i]) - nums.begin();
+            ans.push_back(idx);
         }
         return ans;
     }
