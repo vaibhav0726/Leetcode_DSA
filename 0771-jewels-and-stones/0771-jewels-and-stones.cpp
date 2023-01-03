e32b968e4c92ae7b63d6c0a3a428450fc47dfc15
Vaibhav Varshney <vaibhav.varshney2_cs20@gla.ac.in>
2023-01-03 19:46:22 +0530

    Time: 8 ms (9.57%), Space: 6.2 MB (52.27%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.cpp
+++ 0771-jewels-and-stones/0771-jewels-and-stones.cpp
@@ -1,14 +1,26 @@
 class Solution {
 public:
     int numJewelsInStones(string jewels, string stones) {
-        unordered_map<char, int> mp;
-        for(int i=0; i<jewels.size(); i++){
-            mp[jewels[i]]++;
+        // unordered_map<char, int> mp;
+        // for(int i=0; i<jewels.size(); i++){
+        //     mp[jewels[i]]++;
+        // }
+        // int count = 0;
+        // for(int i=0; i<stones.size(); i++){
+        //     if(mp[stones[i]] != 0) count++;
+        // }
+        // return count;
+        
+        // int res = 0;
+        // unordered_set<char> setJ(J.begin(), J.end());
+        // for (char s : S)
+        //     if (setJ.count(s)) res++;
+        // return res;
+        
+        int num = 0;
+        for (int i = 0; i < jewels.size(); i++){
+            num += count(stones.begin(), stones.end(), jewels[i]);
         }
-        int count = 0;
-        for(int i=0; i<stones.size(); i++){
-            if(mp[stones[i]] != 0) count++;
-        }
-        return count;
+        return num;
     }
 };
