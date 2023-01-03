776b8b35de69b75745446758a98b467e748f13df
Vaibhav Varshney <vaibhav.varshney2_cs20@gla.ac.in>
2023-01-03 18:50:46 +0530

    Time: 8 ms (85.76%), Space: 14.1 MB (9.42%) - LeetHub

---
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int ans = 0;
+        for(int i=0; i<operations.size(); i++){
+            if(operations[i][0] == '-' || operations[i][operations[i].size()-1] == '-'){
+                ans--;
+            }
+            else{
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
