6c43cfefb2087001cb1e55737901d9de7575e26d
Vaibhav Varshney <vaibhav.varshney2_cs20@gla.ac.in>
2023-01-05 20:40:37 +0530

    Time: 531 ms (66.95%), Space: 89.7 MB (68.04%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -6,9 +6,6 @@ private:
 public:
     int findMinArrowShots(vector<vector<int>>& points) {
         sort(points.begin(), points.end(), cmp);
-        for(int i=0; i<points.size(); i++){
-            cout<<points[i][0]<<" "<<points[i][1]<<endl;
-        }
         int arrows = 1;
         int end = points[0][1];
         for(int i=1; i<points.size(); i++){
