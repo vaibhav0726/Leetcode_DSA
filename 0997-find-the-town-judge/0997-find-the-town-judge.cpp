9a012ecf1e101a412cda58c9cf67074c125e8074
Vaibhav Varshney <vaibhav.varshney2_cs20@gla.ac.in>
2023-01-23 19:16:34 +0530

    Time: 184 ms (83.22%), Space: 60.7 MB (95.56%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int findJudge(int N, vector<vector<int>>& trust) {
+        vector<int> balance(N + 1);
+        for (auto &t : trust) --balance[t[0]], ++balance[t[1]];
+        for (auto i = 1; i <= N; ++i) if (balance[i] == N - 1) return i;
+        return -1;
+    }
+};
