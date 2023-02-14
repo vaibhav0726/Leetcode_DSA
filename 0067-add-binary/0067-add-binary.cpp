41d4856f286da63944bb6bfda598aeb8177186b5
Vaibhav Varshney <vaibhav.varshney2_cs20@gla.ac.in>
2023-02-14 23:18:17 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (86.82%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -1,17 +1,37 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-        string s = "";
+//         string s = "";
         
-        int c = 0, i = a.size() - 1, j = b.size() - 1;
-        while(i >= 0 || j >= 0 || c == 1)
-        {
-            c += i >= 0 ? a[i --] - '0' : 0;
-            c += j >= 0 ? b[j --] - '0' : 0;
-            s = char(c % 2 + '0') + s;
-            c /= 2;
+//         int c = 0, i = a.size() - 1, j = b.size() - 1;
+//         while(i >= 0 || j >= 0 || c == 1)
+//         {
+//             c += i >= 0 ? a[i --] - '0' : 0;
+//             c += j >= 0 ? b[j --] - '0' : 0;
+//             s = char(c % 2 + '0') + s;
+//             c /= 2;
+//         }
+        
+//         return s;
+        int i = a.length()-1;
+        int j = b.length()-1;
+        string ans;
+        int carry = 0;
+        
+        while(i>=0 || j>=0 || carry){
+            if(i>=0){
+                carry += a[i] - '0';
+                i--;
+            }
+            if(j>=0){
+                carry += b[j] - '0';
+                j--;
+            }
+            
+            ans += (carry%2 + '0');
+            carry = carry/2;
         }
-        
-        return s;
+        reverse(ans.begin(),ans.end());
+        return ans;
     }
 };
