02bce1134c47f6321dc9abcd7f2e110c3aab186c
Vaibhav Varshney <vaibhav.varshney2_cs20@gla.ac.in>
2023-02-21 18:40:51 +0530

    Time: 23 ms (91.47%), Space: 22.4 MB (62.74%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,10 +1,34 @@
 class Solution {
+    int solve(vector<int>& nums, int low, int high){
+        if(low>high){
+            return 0;
+        }
+        int mid = low + (high - low)/2;
+        if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1] ){
+            return nums[mid];
+        }
+        else{
+            int lower = solve(nums, low, mid-1);
+            int higher = solve(nums, mid+1, high);
+            if(lower != 0) return lower;
+            else if(higher != 0) return higher;
+            else return 0;
+        }       
+    }
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int sum = 0;
-        for(int i=0; i<nums.size(); i++){
-            sum ^= nums[i];
+        if(nums.size() == 1) return nums[0];
+        int ans = solve(nums, 1, nums.size()-2);
+        if(ans != 0){
+            return ans;
         }
-        return sum;
+        else{
+            if(nums[0] != nums[1] ){
+                return nums[0];
+            }
+            else{
+                return nums[nums.size() -1];
+            }
+        }
     }
 };
