3dc6787a587c58a201586ccf87e12639ed888a75
Vaibhav Varshney <vaibhav.varshney2_cs20@gla.ac.in>
2023-03-12 00:43:02 +0530

    Time: 39 ms (56.53%), Space: 32.7 MB (50.21%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -10,34 +10,31 @@
  * };
  */
 class Solution {
-    TreeNode* mi(TreeNode* root){
-        if(root -> left == NULL)
-            return root;
-        else
-            return mi(root -> left);
+    int giveLeftMax(TreeNode* root){
+        if(root == NULL) return INT_MIN;
+        int ma = giveLeftMax(root -> right);
+        return max(ma, root -> val);
     }
 public:
     TreeNode* deleteNode(TreeNode* root, int key) {
-        if(root == NULL){
-            return NULL;
-        } 
-        if (root -> val > key) {
-            root -> left = deleteNode(root -> left, key);
-        } else if (root -> val < key) {
+        if(root == NULL) return NULL;
+        if(root -> val < key){
             root -> right = deleteNode(root -> right, key);
-        } else {
-            if (root -> left == NULL && root -> right == NULL) {
-                return NULL;
-                
-            } else if (root -> left == NULL) { // 1 or 0 child
-                root = root -> right;
-            } else if (root -> right == NULL) { // 1 or 0 child
-                root = root -> left;
-            } else {
-//                 minimum in the right subtreee
-                TreeNode* minNode = mi(root -> right);
-                root -> val = minNode -> val;
-                root -> right = deleteNode(root -> right, minNode -> val);
+        }
+        else if(root -> val > key){
+            root -> left = deleteNode(root -> left, key);
+        }
+        else{
+//             1 or 0 child
+            if(root -> left == NULL)
+                return root -> right;
+            else if(root -> right == NULL)
+                return root -> left;
+            else{
+//                 for both child
+                int lmax = giveLeftMax(root -> left);
+                root -> left = deleteNode(root -> left, lmax);
+                root -> val = lmax;
             }
         }
         return root;
