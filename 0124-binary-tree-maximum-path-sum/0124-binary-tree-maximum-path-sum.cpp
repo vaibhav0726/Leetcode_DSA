f7a847a31803bdff0b034595ee6ce11cafcc5405
Vaibhav Varshney <vaibhav.varshney2_cs20@gla.ac.in>
2023-03-11 15:14:04 +0530

    Time: 30 ms (45.84%), Space: 27.7 MB (37.08%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
@@ -13,17 +13,15 @@ class Solution {
     int sum;
 public:
     int maxPathSum(TreeNode* root) {
-        sum=INT_MIN;
+        sum = INT_MIN;
         help(root);
         return sum;
     }
-    
     int help(TreeNode* root){
-        if(!root)   
-            return 0;
-        int left = max(0, help(root->left));
-        int right = max(0, help(root->right));
-        sum = max(sum, left+right+root->val);
-        return max(left, right)+root->val;
+        if(root == NULL) return 0;
+        int left = max(0, help(root -> left));
+        int right = max(0, help(root -> right));
+        sum = max(sum, left + right + root -> val);
+        return max(left, right) + root -> val;
     }
 };
