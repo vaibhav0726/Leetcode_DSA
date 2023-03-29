23fb0b54f4cc82d555d38ed6fd09e84d8500a802
Vaibhav Varshney <vaibhav.varshney2_cs20@gla.ac.in>
2023-03-29 10:00:29 +0530

    Time: 23 ms (73.58%), Space: 61.6 MB (50.96%) - LeetHub

---
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int longestCycle(int[] edges) {
+        int[] in = new int[edges.length];
+        for (int i = 0; i < edges.length; i++) {
+            if (edges[i] != -1) {
+                in[edges[i]]++;
+            }
+        }
+        boolean[] visited = new boolean[edges.length];
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < in.length; i++) {
+            if (in[i] == 0) {
+                q.add(i);
+            }
+        }
+        while (!q.isEmpty()) {
+            int rv = q.poll();
+            visited[rv] = true;
+            int nbrs = edges[rv];
+            if (nbrs != -1) {
+                in[nbrs]--;
+                if (in[nbrs] == 0) {
+                    q.add(nbrs);
+                }
+            }
+        }
+
+        int ans = -1;
+        for (int i = 0; i < visited.length; i++) {
+            if (!visited[i]) {
+                int count = 1;
+                int nbrs = edges[i];
+                visited[i] = true;
+                while (nbrs != i) {
+                    visited[nbrs] = true;
+                    count++;
+                    nbrs = edges[nbrs];
+                }
+                ans = Math.max(ans, count);
+            }
+        }
+        
+        return ans;
+    }
+}
