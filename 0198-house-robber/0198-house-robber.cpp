504993249959575b1b1351943f1eea2597f32518
Vaibhav Varshney <vaibhav.varshney2_cs20@gla.ac.in>
2023-04-06 09:17:54 +0530

    Time: 5 ms (25.37%), Space: 7.7 MB (62.35%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,12 +1,28 @@
 class Solution {
+//     TLE
+    // int Robber(vector<int> &nums, int i){
+    //     if(i>=nums.size()) return 0;
+    //     int rob = nums[i] + Robber(nums, i+2);
+    //     int DontRob = Robber(nums, i+1);
+    //     return max(rob, DontRob);
+    // }
+    
+    int robberDp(vector<int> &nums, vector<int> &dp, int i){
+        if(i>=nums.size()) return 0;
+        if(dp[i] != -1){
+            return dp[i];
+        }
+        int rob = nums[i] + robberDp(nums, dp, i+2);
+        int DontRob = robberDp(nums, dp, i+1);
+        return dp[i] = max(rob, DontRob);
+    }
 public:
     int rob(vector<int>& nums) {
-        int prev2 = 0, prev = 0, cur = 0;
-        for(auto i : nums) {
-            cur = max(prev, i + prev2);
-            prev2 = prev;
-            prev = cur;
-        }
-        return cur;
+        // return Robber(nums, 0);
+        
+//         use DP for efficient approach
+        vector<int> dp(nums.size(), -1);
+        return robberDp(nums, dp, 0);
+        
     }
 };
