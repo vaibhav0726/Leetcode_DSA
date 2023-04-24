d2747b3b3b8433d442423eea41af91be5783e395
Vaibhav Varshney <vaibhav.varshney2_cs20@gla.ac.in>
2023-04-24 23:42:51 +0530

    Time: 5 ms (28.30%), Space: 7.7 MB (41.71%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.cpp
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
-    int lastStoneWeight(vector<int>& stones) {
-        priority_queue<int> pq;
-        for(auto c: stones){
-            pq.push(c);
-        }
-        while(pq.size() > 1){
+    int lastStoneWeight(vector<int>& A) {
+        priority_queue<int> pq(begin(A), end(A));
+        while (pq.size() > 1) {
             int x = pq.top(); pq.pop();
             int y = pq.top(); pq.pop();
-            if(x-y > 0) pq.push(x-y);
+            if (x > y) pq.push(x - y);
         }
         return pq.empty() ? 0 : pq.top();
     }
