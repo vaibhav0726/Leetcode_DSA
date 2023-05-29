16e1241913ee6028caf53148f870708ea78f8380
Vaibhav Varshney <vaibhav.varshney2_cs20@gla.ac.in>
2023-05-29 23:55:39 +0530

    Time: 67 ms (39.62%), Space: 33.1 MB (28.39%) - LeetHub

--- 1603-design-parking-system/1603-design-parking-system.cpp
+++ 1603-design-parking-system/1603-design-parking-system.cpp
@@ -16,9 +16,3 @@ public:
         }
     }
 };
-
-/**
- * Your ParkingSystem object will be instantiated and called as such:
- * ParkingSystem* obj = new ParkingSystem(big, medium, small);
- * bool param_1 = obj->addCar(carType);
- */
