8053e67a7c8eb5bc90e3de8eb3077a073ca0829b
Vaibhav Varshney <vaibhav.varshney2_cs20@gla.ac.in>
2023-06-01 00:17:45 +0530

    Time: 160 ms (55.95%), Space: 58.9 MB (50.81%) - LeetHub

---
+++ 1396-design-underground-system/1396-design-underground-system.cpp
@@ -0,0 +1,29 @@
+class UndergroundSystem {
+private:
+    unordered_map<int, pair<string, int>> checkInIn;
+    unordered_map<string, pair<int, int>> path;
+public:
+    UndergroundSystem() {
+        
+    }
+    
+    void checkIn(int id, string stationName, int t) {
+        checkInIn[id]= {stationName, t};
+    }
+    
+    void checkOut(int id, string stationName, int t) {
+        auto checkIn_temp = checkInIn[id];
+        checkInIn.erase(id);
+        
+        string path_name = checkIn_temp.first + "_" + stationName;
+        // int new_time = checkIn_temp + t;
+        path[path_name].first += t - checkIn_temp.second;
+        path[path_name].second += 1; 
+    }
+    
+    double getAverageTime(string startStation, string endStation) {
+        string path_name = startStation + "_" + endStation;
+        auto &route = path[path_name];
+        return (double) route.first / route.second;
+    }
+};
