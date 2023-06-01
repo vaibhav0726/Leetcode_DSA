af679e0567e45f57b051c593620591bcdaa5e3c1
Vaibhav Varshney <vaibhav.varshney2_cs20@gla.ac.in>
2023-06-02 01:41:27 +0530

    Time: 158 ms (20.22%), Space: 36.9 MB (16.09%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,36 +1,45 @@
 class Solution {
 public:
     // Helper function to check if point is in grid
-    bool isValidPoint(int x, int y, int n, int m) {
-        return x >= 0 && x <= n && y >= 0 && y <= m;
+    bool isValidPoint(int x, int y, int n) {
+        return x >= 0 && x < n && y >= 0 && y < n;
     }
     
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int n = grid.size()-1, m = grid[0].size()-1;
-        if (grid[0][0] || grid[n][m]) 
+        int n = grid.size();
+        if (grid[0][0] || grid[n-1][n-1]) 
             return -1;
         
-        queue<vector<int> > q;
-        vector<int> curr;
+        queue<vector<int>> q;
         
-        q.push({0, 0});
+        q.push({0, 0, 1});
         grid[0][0] = 1;
         
-        while (!q.empty() && !grid[n][m]) {
-            curr = q.front();
-            q.pop();
-            
-            for (int i = 0; i < 8; i++) {
-                int x = curr[0] + x_points[i];
-                int y = curr[1] + y_points[i];
-                
-                if (isValidPoint(x, y, n, m) && grid[x][y] == 0) {
-                    grid[x][y] = grid[curr[0]][curr[1]] + 1;
-                    q.push({x, y});
+        while (!q.empty()) {
+            int si = q.size();
+            while(si--){
+                // cout << "hello" << endl;
+                vector<int> curr = q.front();
+                q.pop();
+
+                if(curr[0] == n-1 && curr[1] == n-1){
+                    return curr[2];
+                }
+
+                for (int i = 0; i < 8; i++) {
+                    int r = curr[0] + x_points[i];
+                    int c = curr[1] + y_points[i];
+                        // cout << r << " " << c << endl;
+
+                    if (isValidPoint(r, c, n) && grid[r][c] == 0) {
+                        // cout << r << " " << c << endl;
+                        grid[r][c] = 1;
+                        q.push({r, c, curr[2] + 1});
+                    }
                 }
             }
         }
-        return grid[n][m] ? grid[n][m] : -1;
+        return -1;
     }
     
 private:
