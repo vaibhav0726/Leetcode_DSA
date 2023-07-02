0635c3718664149200548bdf6697455e99077edd
Vaibhav Varshney <vaibhav.varshney2_cs20@gla.ac.in>
2023-07-03 02:00:45 +0530

    Time: 7 ms (31.31%), Space: 11.1 MB (14.86%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,36 +1,29 @@
 class Solution {
-private:
-    int pivot(vector<int>& nums){
-        int low=0, high = nums.size()-1;
-        while(low < high){
-            int mid= low + (high - low)/2;
-            if(nums[mid] < nums[0]){
-                high = mid;
-            }
-            else{
-                low = mid +1;
-            }
-        }
-        return low;
-    }
-    int binary_search(vector<int>& nums, int target, int low, int high){
-        while(low <= high){
-            int mid = low + (high - low)/2;
-            if(nums[mid] == target) return mid;
-            else if(nums[mid] > target) high = mid-1;
-            else low = mid+1;
-        }
-        return -1;
-    }
 public:
     int search(vector<int>& nums, int target) {
-        int piv = pivot(nums);
-        int n = nums.size();
-        if(nums[piv] <= target && target <= nums[n-1]){
-            return binary_search(nums, target, piv, n-1);
+        int low = 0, high = nums.size()-1;
+        while(low <= high){
+            int mid = (low + high)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            if(nums[low] <= nums[mid]){
+                if(target >= nums[low] && target < nums[mid]){
+                    high = mid - 1;
+                }
+                else{
+                    low = mid + 1;
+                }
+            }
+            else{
+                if(target > nums[mid] && target <= nums[high]){
+                    low = mid + 1;
+                }
+                else{
+                    high = mid - 1;
+                }
+            }
         }
-        else{
-            return binary_search(nums, target, 0, piv-1);
-        }
+        return -1;
     }
 };
