5c45c435930c6bdf216f65ce4ef0334b53c3e5d0
Vaibhav Varshney <vaibhav.varshney2_cs20@gla.ac.in>
2023-07-07 00:10:47 +0530

    Time: 10 ms (18.76%), Space: 9.7 MB (13.06%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -3,19 +3,17 @@ public:
     int searchInsert(vector<int>& nums, int target) {
         int low = 0, high = nums.size()-1;
         while(low <= high){
-            int mid = low + (high-low)/2;
-            if(nums[mid] == target)
+            int mid = low + (high - low)/2;
+            if(nums[mid] == target){
                 return mid;
-            if(nums[mid] < target)
-                low = mid +1;
-            else
-                high = mid-1;
-        }
-        for(int i=0; i<nums.size(); i++){
-            if(nums[i] > target){
-                return i;
+            }
+            if(nums[mid] < target){
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
             }
         }
-        return nums.size();
+        return low;
     }
 };
